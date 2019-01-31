f561849d101ed65f1bb00e4fbde9e9400ba711e4
Verta-master <vertyagina@gmail.com>
2019-01-31 14:09:38 +0600

    Update 2019-01-31T08:09:33.466Z

--- js/script.js
+++ js/script.js
@@ -7,64 +7,55 @@ $('.menu__btn').click(function() {
   $('.menu__list').slideToggle();
 })
 
-if (window.innerWidth < 766) {
-  $('.menu__link').click(function() {
-    $('.menu__list').slideToggle();
-    $('.menu').removeClass('menu--opened');
-  })
-}
-
 //Scroll to top
-$(window).scroll(function(){
-  if ((window.pageYOffset || document.documentElement.scrollTop ||document.body.scrollTop) >= 100) {
-    $(".btn-top").fadeIn();
-  } else {
-    $(".btn-top").fadeOut();
-  };
-});
+//$(window).scroll(function(){
+//  if ((window.pageYOffset || document.documentElement.scrollTop ||document.body.scrollTop) >= 100) {
+//    $(".btn-top").fadeIn();
+//  } else {
+//    $(".btn-top").fadeOut();
+//  };
+//});
+//
+//$(".btn-top").click(function () {
+//  $("body,html").animate({
+//    scrollTop: 0
+//  }, 400);
+//  return false;
+//});
 
-$(".btn-top").click(function () {
-  $("body,html").animate({
-    scrollTop: 0
-  }, 400);
-  return false;
-});
-
-//Scroll to menu anchor
-$(document).ready(function () {
-  $(document).on("scroll", onScroll);
-
-  //smoothscroll
-  $('.menu__item a[href^="#"]').on('click', function (e) {
-    e.preventDefault();
-    $(document).off("scroll");
-
-    $('.menu__item a').each(function () {
-        $(this).removeClass('menu__link--active');
-    })
-    $(this).addClass('menu__link--active');
-
-    var target = this.hash,
-        menu = target;
-    $target = $(target);
-    $('html, body').stop().animate({
-        'scrollTop': $target.offset().top
-    }, 500, 'swing', function () {
-        $(document).on("scroll", onScroll);
+//digit animation
+$(function() {
+    var num = $(".digit__value");
+    num.each(function(indx, el) {
+        var max = $(el).data("max");
+        var duration = 1000;
+        var visibility = checkViewport(el);
+        $(el).on("animeNum", function() {
+            $({n: 0}).animate({n: max}, {
+                easing: "linear",
+                duration: duration,
+                step: function(now, fx) {
+                    $(el).html(now | 0)
+                }
+            })
+        }).data("visibility", visibility);
+        visibility && $(el).trigger("animeNum")
     });
-  });
+ 
+    function checkViewport(el) {
+    var H = document.documentElement.clientHeight,
+        h = el.scrollHeight,
+        pos = el.getBoundingClientRect();
+        return pos.top + h > 0 && pos.bottom - h < H
+    }
+    $(window).scroll(function() {
+        num.each(function(indx, el) {
+            var visibility = checkViewport(el);
+            el = $(el);
+            var old = el.data("visibility");
+            old != visibility && el.data("visibility", visibility) && !old && el.trigger("animeNum")
+        })
+    })
 });
 
-function onScroll(event){
-  var scrollPos = $(document).scrollTop();
-  $('.menu__link').each(function () {
-      event.preventDefault();
-      var currLink = $(this);
-      var refElement = $(currLink.attr("href"));
-      if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-          $('.menu__link').removeClass("menu__link--active");
-          currLink.addClass("menu__link--active");
-      }
-  });
-};
 });
